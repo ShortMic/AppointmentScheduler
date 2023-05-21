40a8731188fa22c3744aa65c01a730f87ffb4a39
ShortMic <44100168+ShortMic@users.noreply.github.com>
2023-05-21 13:34:19 -0700

    Added templates for controllers and views for add, modify functionality to appointments and customers. Added Reports controller and view template.

---
+++ src/main/java/Application/Controllers/ModifyCustomerController.java
@@ -0,0 +1,31 @@
+package Application.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableView;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ModifyCustomerController implements Initializable{
+
+    @FXML
+    public Text appTitleLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    public void onSubmit(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onCancel(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/Application/Controllers/ReportsController.java
@@ -0,0 +1,28 @@
+package Application.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ReportsController implements Initializable{
+
+    @FXML
+    public Text appTitleLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    public void onSubmit(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onCancel(ActionEvent actionEvent) {
+    }
+}
