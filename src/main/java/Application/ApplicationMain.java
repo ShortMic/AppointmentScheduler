c91577afd8edb6c72bfdc91dc95ecd9bbc2ec936
ShortMic <44100168+ShortMic@users.noreply.github.com>
2023-05-21 11:39:30 -0700

    Added english/french key value translation to translation_fr.properties, removed translation testing from ApplicationMain and implemented translation helper functions in LoginController and translated all labels, text, titles and error messaging based on fr locale settings.

--- src/main/java/Application/ApplicationMain.java
+++ src/main/java/Application/ApplicationMain.java
@@ -9,13 +9,8 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Locale;
-import java.util.ResourceBundle;
 
 public class ApplicationMain extends Application {
-
-    public static Locale locale;
-    public static final boolean DEBUG = true;
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -27,17 +22,6 @@ public class ApplicationMain extends Application {
     }
 
     public static void main(String[] args) throws SQLException {
-        //Locale settings
-        //https://wgu.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=5251c286-743e-410d-b7c9-ab4901120742
-        if(DEBUG){Locale.setDefault(new Locale("fr"));}
-        locale = Locale.getDefault();
-        if(!locale.toString().equals("en_US")){
-            System.out.println("Application language non-default mode.");
-            ResourceBundle resourceBundle = ResourceBundle.getBundle("Application/Translation_fr", locale);
-            System.out.println(resourceBundle.getString("Hello"));
-        }else{
-            System.out.println("Application language default mode.");
-        }
         JDBC.openConnection();
         /*
         int rowsAffected = UserQuery.update(3,"Michael", "Alligator1");
--- src/main/java/Application/Controllers/LoginController.java
+++ src/main/java/Application/Controllers/LoginController.java
@@ -13,13 +13,15 @@ import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
 
+    public static Locale locale;
+    public static final boolean DEBUG = false;
+    private ResourceBundle rb;
     private Alert alert;
-    private String localeCountry;
-    private String localeLanguage;
     @FXML
     public TextField userNameTextbox;
     @FXML
@@ -41,19 +43,79 @@ public class LoginController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        localeCountry = ApplicationMain.locale.getCountry();
-        localeLanguage = ApplicationMain.locale.getLanguage();
+        //Locale settings
+        //https://wgu.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=5251c286-743e-410d-b7c9-ab4901120742
+        rb = resourceBundle;
+        if(DEBUG){Locale.setDefault(new Locale("fr","FR"));}
+        locale = Locale.getDefault();
+        String localeCountry = locale.getCountry();
+        String localeLanguage = locale.getLanguage();
+        if(!locale.toString().equals("en_US")){
+            System.out.println("Application language non-default mode.");
+            rb = ResourceBundle.getBundle("Application/Translation_fr", locale);
+            System.out.println(rb.getString("Hello"));
+            loginLabel.setText(translate(loginLabel));
+            appTitleLabel.setText(translate(appTitleLabel));
+            passwordLabel.setText(translate(passwordLabel));
+            userNameLabel.setText(translate(userNameLabel));
+            localeLabel.setText(translate(localeLabel));
+            submitCredentialsButton.setText(translate(submitCredentialsButton.getText()));
+        }else{
+            System.out.println("Application language default mode.");
+        }
         if(localeCountry.isEmpty()){
             System.out.println("Locale unrecognized! Defaulting country to US");
         }else{
-            zoneIdLabel.setText(ApplicationMain.locale.toString());
+            zoneIdLabel.setText(locale.toString());
         }
+    }
+
+    private String translate(Text t){
+        String[] str = t.getText().toString().split("\\s+");
+        String translation = "";
+        for(String s: str){
+            try{
+                translation += rb.getString(s)+" ";
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+                translation += s+" ";
+            }
+        }
+        return translation;
+    }
+
+    private String translate(Label t){
+        String[] str = t.getText().toString().split("\\s+");
+        String translation = "";
+        for(String s: str){
+            try{
+                translation += rb.getString(s)+" ";
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+                translation += s+" ";
+            }
+        }
+        return translation;
+    }
+
+    private String translate(String t){
+        String[] str = t.split("\\s+");
+        String translation = "";
+        for(String s: str){
+            try{
+                translation += rb.getString(s)+" ";
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+                translation += s+" ";
+            }
+        }
+        return translation;
     }
 
     @FXML
     public void onLoginSubmit(ActionEvent actionEvent) throws SQLException {
-        System.out.println(actionEvent.getSource().toString()+": "+actionEvent.getEventType().getName().toString());
-        System.out.println(actionEvent.toString());
+//        System.out.println(actionEvent.getSource().toString()+": "+actionEvent.getEventType().getName().toString());
+//        System.out.println(actionEvent.toString());
         int userId = 0;
         try{
             userId = UserQuery.select(userNameTextbox.getText(), passwordTextbox.getText());
@@ -61,13 +123,13 @@ public class LoginController implements Initializable {
                 System.out.println("User and password accepted!");
             }else if(userId == -1){
                 alert = new Alert(Alert.AlertType.ERROR,
-                        "Invalid username and/or password!");
-                alert.setHeaderText("Login failed");
+                        translate("Invalid username and/or password!"));
+                alert.setHeaderText(translate("Login failed"));
                 alert.show();
             }else{
                 alert = new Alert(Alert.AlertType.ERROR,
-                        "Something went wrong with credential verification!");
-                alert.setHeaderText("Login failed");
+                        translate("Something went wrong with credential verification!"));
+                alert.setHeaderText(translate("Login failed"));
                 alert.show();
                 System.out.println("Unexpected user id match outcome from Users table!");
             }
