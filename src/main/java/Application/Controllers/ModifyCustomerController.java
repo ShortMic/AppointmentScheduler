6d76cbcd8c9ba77444e3b347de4541497aa0f7d3
ShortMic <44100168+ShortMic@users.noreply.github.com>
2023-06-14 10:27:08 -0700

    Update ModifyCustomerController.java
    Copied over AddCustomerController functions and logic. TODO: work on pre-population of fields.

--- src/main/java/Application/Controllers/ModifyCustomerController.java
+++ src/main/java/Application/Controllers/ModifyCustomerController.java
@@ -1,35 +1,210 @@
 package Application.Controllers;
 
-import Application.Models.CustomerTable;
+import Application.ApplicationMain;
+import Application.Models.*;
+import Application.Repository.AppointmentsCache;
+import Application.Repository.CountryCache;
+import Application.Repository.CustomersCache;
+import Application.Repository.DivisionLevelCache;
+import Utilities.AppointmentQuery;
+import Utilities.CustomerQuery;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TableView;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ModifyCustomerController implements Initializable{
-//TODO: Implement ModifyCustomerController
-
-    public static CustomerTable selectedCustomer;
-
     @FXML
-    public Text appTitleLabel;
-
+    public TextField customerIDTextField;
+    public Label stateLabel;
+    public TextField nameTextField;
+    public TextField addressTextField;
+    public TextField zipTextField;
+    public TextField phoneTextField;
+    public ComboBox<Country> countryMenuBtn;
+    public ComboBox<DivisionLevel1> stateMenuBtn;
+    public Button addCustomerBtn;
+    public Button cancelCustomerBtn;
+    private boolean countryMenuModifiedFlag = false;
+    private String errorMsg = "";
+    private boolean errorFlag = false;
+    public Alert alert;
+    public static CustomerTable selectedCustomer;
+    
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        try{
+            countryMenuBtn.setItems(CountryCache.getInstance().getCache());
+            countryMenuBtn.setConverter(new StringConverter<Country>() {
+                @Override
+                public String toString(Country country) {
+                    return country == null ? null : country.getCountry();
+                }
 
+                @Override
+                public Country fromString(String s) {
+                    try {
+                        return CountryCache.getInstance().getCache().stream().filter(x -> x.getCountry().equals(s)).findFirst().orElse(null);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }
+            });
+        }catch(Exception exception){
+            System.out.println(exception.getMessage());
+        }
     }
 
-    @FXML
-    public void onSubmit(ActionEvent actionEvent) {
+    public void onStartTimeMenuBtn(ActionEvent actionEvent) {
     }
 
-    @FXML
-    public void onCancel(ActionEvent actionEvent) {
+    public void onEndTimeMenuBtn(ActionEvent actionEvent) {
+    }
+
+    public void onStartDateField(ActionEvent actionEvent) {
+    }
+
+    public void onEndDateField(ActionEvent actionEvent) {
+    }
+
+    public void onCancelCustomerBtn(ActionEvent actionEvent) throws IOException {
+        ((Stage)(((Button)actionEvent.getSource()).getScene().getWindow())).setScene(new Scene(new FXMLLoader(ApplicationMain.class.getResource("MainMenuView.fxml")).load(), 1070, 564));
+    }
+
+    public void onAddCustomerBtn(ActionEvent actionEvent) {
+        try{
+            textFieldDataValidationLogger("Name", nameTextField, "String");
+            textFieldDataValidationLogger("Address", addressTextField, "String");
+            textFieldDataValidationLogger("Zipcode", zipTextField, "String");
+            textFieldDataValidationLogger("Phone", phoneTextField, "String");
+            if(errorFlag){
+                alert = new Alert(Alert.AlertType.ERROR, errorMsg);
+                alert.setHeaderText("Invalid Input");
+                alert.show();
+                errorMsg = "";
+                errorFlag = false;
+            }else{
+                errorFlag = (countryMenuBtn.getSelectionModel().getSelectedItem() == null || stateMenuBtn.getSelectionModel().getSelectedItem() == null);
+                if(errorFlag){
+                    alert = new Alert(Alert.AlertType.ERROR, "Combo Box Field(s) not entered. Please make a selection!");
+                    alert.setHeaderText("Missing Required Field");
+                    alert.show();
+                    errorFlag = false;
+                }else{
+                    Customer customer = new Customer(-1, nameTextField.getText(), addressTextField.getText(),
+                            zipTextField.getText(), phoneTextField.getText(), stateMenuBtn.getSelectionModel().getSelectedItem().getDivisionId());
+                    customer.setCustomerId(CustomerQuery.create(customer));
+                    CustomersCache.getInstance().getCache().add(new CustomerTable(customer, countryMenuBtn.getSelectionModel().getSelectedItem().getCountry(), stateMenuBtn.getSelectionModel().getSelectedItem().getDivision()));
+                    ((Stage)(((Button)actionEvent.getSource()).getScene().getWindow())).setScene(new Scene(new FXMLLoader(ApplicationMain.class.getResource("MainMenuView.fxml")).load(), 1070, 564));
+                }
+            }
+        }catch(Exception exception){
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    private void textFieldDataValidationLogger(String textFieldLabel, TextField textField, String expectedType){
+        String message = "";
+        switch(expectedType){
+            case "int":
+                if(!tryParseInt(textField.getText())){
+                    message = "Invalid type for "+textFieldLabel+" field: "+"\""+textField.getText()+"\" is not an "+expectedType;
+                }
+                break;
+            case "Double":
+                if(!tryParseDouble(textField.getText())){
+                    message = "Invalid type for "+textFieldLabel+": "+"\""+textField.getText()+"\" is not a "+expectedType;
+                }
+                break;
+            case "String":
+                if(textField.getText().isEmpty()){
+                    message = "Required field "+textFieldLabel+" is empty!";
+                }
+            default:
+                break;
+        }
+        if(!message.equals("")){
+            errorMsg += message+" ";
+            errorFlag = true;
+            System.out.println(message);
+        }
+    }
+
+    /**
+     * Private helper method which returns whether a string is convertible to an int.
+     * @param text  The text to try converting into an int
+     * @return boolean
+     */
+    private boolean tryParseInt(String text){
+        try{
+            Integer.parseInt(text);
+        }catch(Exception exception){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Private helper method which returns whether a string is convertible to a double.
+     * @param text  The text to try converting into a double
+     * @return boolean
+     */
+    private boolean tryParseDouble(String text){
+        try{
+            Double.parseDouble(text);
+        }catch(Exception exception){
+            return false;
+        }
+        return true;
+    }
+
+    public void onSelectCountryBtn(ActionEvent actionEvent) throws SQLException {
+        Country selectedCountry = ((ComboBox<Country>)actionEvent.getSource()).getSelectionModel().getSelectedItem();
+        if(selectedCountry != null){
+            stateMenuBtn.setDisable(false);
+            populateStateMenuBtn(selectedCountry.getCountryId());
+            countryMenuModifiedFlag = true;
+        }else if(countryMenuModifiedFlag){
+            stateMenuBtn.getSelectionModel().clearSelection();
+        }
+    }
+
+    private void populateStateMenuBtn(int countryId) throws SQLException {
+        List<DivisionLevel1> filteredCountryStates = DivisionLevelCache.getInstance().getCache().stream().filter(x -> x.getCountryId() == countryId).toList();
+        ObservableList<DivisionLevel1> countryStates = FXCollections.observableArrayList(new ArrayList<DivisionLevel1>(filteredCountryStates));
+        stateMenuBtn.setItems(countryStates);
+        stateMenuBtn.setConverter(new StringConverter<DivisionLevel1>() {
+            @Override
+            public String toString(DivisionLevel1 divisionLevel1) {
+                return divisionLevel1 == null ? null : divisionLevel1.getDivision();
+            }
+
+            @Override
+            public DivisionLevel1 fromString(String s) {
+                try {
+                    return DivisionLevelCache.getInstance().getCache().stream().filter(x -> x.getDivision().equals(s)).findFirst().orElse(null);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        });
     }
 }
